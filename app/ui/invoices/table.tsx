c81debd654fa73f25543685e7a0759e5896559ee
David Hidalgo <juandak16@gmail.com>
2023-11-22 18:34:45 -0400

    fix: add image alt

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt='Profile picture of {invoice.name}'
                       />
                       <p>{invoice.name}</p>
                     </div>
